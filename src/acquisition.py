33f496ee020cfb65aac0b2e9d5d8abec7d6db96d
Arie Blumenzweig <theblumz@gmail.com>
2025-05-07 13:58:38 +0300

    black

--- src/acquisition.py
+++ src/acquisition.py
@@ -13,22 +13,24 @@ from common.filer import Filer
 from typing import Dict, Optional
 from guiding import GuidingMode
 
-logger = logging.getLogger('mast.unit.' + __name__)
+logger = logging.getLogger("mast.unit." + __name__)
 filer = Filer(logger)
 init_log(logger)
 
 
 class Acquisition:
-    def __init__(self, unit: 'Unit',
-                 approach_mode: int,
-                 solver_id: SolverId,
-                 make_corrections: bool = True,
-                 target_ra: Optional[float] = None,
-                 target_dec: Optional[float] = None,
-                 conf: Optional[Dict] = None,
-                 skip_sky: bool = False,
-                 guiding_mode: GuidingMode = GuidingMode.PlateSolving
-                 ):
+    def __init__(
+        self,
+        unit: "Unit",
+        approach_mode: int,
+        solver_id: SolverId,
+        make_corrections: bool = True,
+        target_ra: Optional[float] = None,
+        target_dec: Optional[float] = None,
+        conf: Optional[Dict] = None,
+        skip_sky: bool = False,
+        guiding_mode: GuidingMode = GuidingMode.PlateSolving,
+    ):
         if not conf:
             raise Exception(f"Acquisition: conf == None")
 
@@ -43,36 +45,43 @@ class Acquisition:
             self.slew_to_target = True
         else:
             st = self.unit.mount.status()
-            self.target_ra = st['ra_j2000_hours ']
-            self.target_dec = st['dec_j2000_degs ']
+            self.target_ra = st["ra_j2000_hours "]
+            self.target_dec = st["dec_j2000_degs "]
 
         self.conf = conf
-        self.ra_tolerance = conf['tolerance']['ra_arcsec']
-        self.dec_tolerance = conf['tolerance']['dec_arcsec']
+        self.ra_tolerance = conf["tolerance"]["ra_arcsec"]
+        self.dec_tolerance = conf["tolerance"]["dec_arcsec"]
         self.corrections: Dict[str, Corrections] = {}
         self.folder = PathMaker().make_acquisition_folder(
             tags={
-                'target': f"{target_ra},{target_dec}",
-            })
+                "target": f"{target_ra},{target_dec}",
+            }
+        )
         self.skip_sky = skip_sky
         self.guiding_mode = guiding_mode
 
     def save_corrections(self, phase: str):
         if phase in self.corrections:
-            path = os.path.join(self.folder, phase, 'corrections.json')
+            path = os.path.join(self.folder, phase, "corrections.json")
             for i in range(3):
                 try:
-                    with open(path, 'w') as fp:
+                    with open(path, "w") as fp:
                         json.dump((self.corrections[phase]).to_dict(), fp, indent=2)
                         break
                 except Exception as e:
                     logger.error(f"failed to write {path} (error: {e})")
                     continue
-            plot_phase_corrections(phase=phase, corrections=self.corrections[phase], file=path,
-                                   ends_of_phases=[datetime.datetime.now(datetime.UTC)])
+            plot_phase_corrections(
+                phase=phase,
+                corrections=self.corrections[phase],
+                file=path,
+                ends_of_phases=[datetime.datetime.now(datetime.UTC)],
+            )
             time.sleep(2)
             filer.move_ram_to_shared(path)
-            filer.move_ram_to_shared(path.replace('json', 'png'))
+            filer.move_ram_to_shared(path.replace("json", "png"))
 
     def post_process(self):
-        plot_acquisition_corrections(self.folder.replace(filer.ram.root, filer.shared.root))
+        plot_acquisition_corrections(
+            self.folder.replace(filer.ram.root, filer.shared.root)
+        )
