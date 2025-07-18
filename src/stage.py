6e19498034ab0381bfeed9b606f157f8cf66ebbc
blumzi <theblumz@gmail.com>
2025-07-18 10:29:51 +0300

    added activity details

--- src/stage.py
+++ src/stage.py
@@ -360,7 +360,7 @@ class Stage(Component, SwitchedOutlet):
             assert ximclib, "No ximclib"
             result = ximclib.command_move(self.device, value)
         if result == Result.Ok:
-            self.start_activity(StageActivities.Moving)
+            self.start_activity(StageActivities.Moving, details=f"target={self.target}")
         else:
             raise Exception(f"Could not start move to {value} ({result=})")
 
@@ -529,7 +529,7 @@ class Stage(Component, SwitchedOutlet):
         self.target = position
         self.motion_start_time = datetime.datetime.now()
         logger.info(f"{op}: move: from {self.position=} to {self.target=}")
-        self.start_activity(StageActivities.Moving)
+        self.start_activity(StageActivities.Moving, details=f"from {self.position} to {self.target}")
 
         return CanonicalResponse_Ok
 
