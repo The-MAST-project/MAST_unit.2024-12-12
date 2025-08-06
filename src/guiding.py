110dac7a9c5264bbd6f0bb6d794969e2244b6459
blumzi <theblumz@gmail.com>
2025-08-06 13:55:51 +0300

    cleanup

--- src/guiding.py
+++ src/guiding.py
@@ -5,7 +5,7 @@ from typing import TYPE_CHECKING
 
 from common.activities import ImagerActivities, UnitActivities
 from common.canonical import CanonicalResponse, CanonicalResponse_Ok
-from common.config import ImagerBinningConfig
+from common.config import Config, ImagerBinningConfig
 from common.utils import function_name
 # from phd2.phd2 import PHD2Connector
 from solving_guider import SolvingGuider
