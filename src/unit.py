7088e03a486e873099ed958661a30d314eba5f79
Arie Blumenzweig <theblumz@gmail.com>
2026-08-05 10:19:49 +0300

    cleanups

--- src/unit.py
+++ src/unit.py
@@ -1,7 +1,7 @@
-import logging
 import datetime
 import io
 import ipaddress
+import logging
 import os
 import socket
 import threading
@@ -19,10 +19,10 @@ from fastapi.routing import APIRouter
 from PIL import Image
 from starlette.websockets import WebSocket, WebSocketDisconnect
 
-import common.asi as asi
 from acquirer import Acquirer
 from acquisition import Acquisition
 from autofocusing import Autofocuser, AutofocusResult
+from common import asi
 from common.activities import (
     CoverActivities,
     FocuserActivities,
@@ -59,7 +59,6 @@ from phd2.phd2 import PHD2Connector
 from PlaneWave import pwi4_client
 from solving import Solver
 from stage import Stage
-from common.mast_logging import get_logger
 
 RA_REGEX = r"^(\d{1,2}):(\d{2}):(\d{2}(?:\.\d{1,3})?)$"
 DEC_REGEX = r"^([+-]?)(\d{1,2}):(\d{2}):(\d{2}(?:\.\d{1,3})?)$"
@@ -78,14 +77,14 @@ def configured_imager() -> ImagerTypes | None:
     return ImagerTypes(t)
 
 
-def get_imager_type(
-    imager_type: ImagerTypes = Query(default=configured_imager()),
-) -> ImagerTypes:
-    t = imager_type
-    if not t:
-        t = configured_imager()
-        assert t is not None, "No imager type configured"
-    return t
+# def get_imager_type(
+#     imager_type: ImagerTypes = Query(default=configured_imager()),
+# ) -> ImagerTypes:
+#     t = imager_type
+#     if not t:
+#         t = configured_imager()
+#         assert t is not None, "No imager type configured"
+#     return t
 
 
 class GuideDirections(Enum):
@@ -276,7 +275,7 @@ class Unit(Component):
 
     @property
     def connected(self):
-        return all([comp.connected for comp in self.components])
+        return all(comp.connected for comp in self.components)
 
     @connected.setter
     def connected(self, value: bool):
@@ -340,22 +339,22 @@ class Unit(Component):
 
         all_corrections: list = []
 
-        if self.acquirer and self.acquirer.latest_acquisition:
-            corrections_list = (
-                self.acquirer.latest_acquisition.corrections
-                if (self.acquirer.latest_acquisition and self.acquirer.latest_acquisition.corrections)
-                else []
-            )
+        # if self.acquirer and self.acquirer.latest_acquisition:
+        #     corrections_list = (
+        #         self.acquirer.latest_acquisition.corrections
+        #         if (self.acquirer.latest_acquisition and self.acquirer.latest_acquisition.corrections)
+        #         else []
+        #     )
 
-            # for phase in list(get_args(Const.CorrectionPhase)):
-            #     if isinstance(corrections_list, dict):
-            #         if phase not in corrections_list:
-            #             corrections_list[phase] = Corrections(phase=phase)
-            #         correction = corrections_list[phase]
-            #         if isinstance(correction, list):
-            #             all_corrections.extend(correction)
-            #         else:
-            #             all_corrections.append(correction)
+        #     for phase in list(get_args(Const.CorrectionPhase)):
+        #         if isinstance(corrections_list, dict):
+        #             if phase not in corrections_list:
+        #                 corrections_list[phase] = Corrections(phase=phase)
+        #             correction = corrections_list[phase]
+        #             if isinstance(correction, list):
+        #                 all_corrections.extend(correction)
+        #             else:
+        #                 all_corrections.append(correction)
 
         ret = FullUnitStatus(
             **self.component_status().model_dump(),
@@ -438,7 +437,7 @@ class Unit(Component):
             self._was_shut_down = True
 
         # UnitActivities.AutofocusingPWI4
-        if self.is_active(UnitActivities.AutofocusingPWI4):
+        if self.pw is not None and self.autofocuser is not None and self.is_active(UnitActivities.AutofocusingPWI4):
             autofocus_status = self.pw.status().autofocus
             if not autofocus_status:
                 logger.error("Empty PWI4 autofocus status")
@@ -482,7 +481,7 @@ class Unit(Component):
                     logger.error("PlaneWave autofocus failed")
                     self.autofocus_result.best_position = None
                     self.autofocus_result.tolerance = None
-                self.autofocus_result.time_stamp = datetime.datetime.now().isoformat()
+                self.autofocus_result.time_stamp = datetime.datetime.now(tz=datetime.UTC).isoformat()
 
                 self.end_activity(UnitActivities.AutofocusingPWI4)
             else:
@@ -503,7 +502,7 @@ class Unit(Component):
         components = set(self.components)
         if self.unit_conf and self.unit_conf.name.lower() == "mastw":
             components.discard(self.covers)
-        return all([c.operational for c in components])
+        return all(c.operational for c in components)
 
     @property
     def why_not_operational(self) -> list[str]:
@@ -615,6 +614,9 @@ class Unit(Component):
         ] = None,
     ) -> CanonicalResponse:
 
+        if self.imager is None:
+            return CanonicalResponse(errors=["imager is not initialized"])
+
         if ra_j2000_hours:
             if isinstance(ra_j2000_hours, str):
                 if ":" in ra_j2000_hours:
@@ -685,7 +687,7 @@ class Unit(Component):
         ).start()
         return CanonicalResponse_Ok
 
-    def do_expose(  # noqa: C901
+    def do_expose(
         self,
         subfolder: str | None = None,
         exposure_seconds: float = 3,
@@ -712,7 +714,7 @@ class Unit(Component):
         for repeat in range(repeats):
             end = None
             if seconds_between_exposures != 0.0:
-                start = datetime.datetime.now()
+                start = datetime.datetime.now(tz=datetime.UTC)
                 end = start + datetime.timedelta(seconds=seconds_between_exposures)
 
             unit_roi = UnitRoi(fiber_x, fiber_y, width, height)
@@ -738,7 +740,7 @@ class Unit(Component):
                 filer.move_ram_to_shared(self.imager.latest_settings.image_path)
 
             if end is not None and seconds_between_exposures != 0.0:
-                now = datetime.datetime.now()
+                now = datetime.datetime.now(tz=datetime.UTC)
                 if now < end:
                     period = (end - now).seconds
                     logger.info(f"{op}: sleeping {period} seconds till next exposure ...")
@@ -809,7 +811,7 @@ class Unit(Component):
         phd2 = self.guider._backend
 
         if isinstance(self.guider._backend, PHD2Connector) and not isinstance(self.imager._backend, PHD2Connector):
-            start = datetime.datetime.now()
+            start = datetime.datetime.now(tz=datetime.UTC)
             logger.debug(msg + "telling phd2 to disconnect equipment")
             phd2.disconnect_equipment()
             while phd2.equipment_is_connected():
@@ -820,7 +822,11 @@ class Unit(Component):
             self.guider._backend.connect_equipment()
             while not phd2.equipment_is_connected():
                 time.sleep(0.1)
-            logger.debug(f"phd2 disconnect/connect took {humanfriendly.format_timespan(datetime.datetime.now() - start)}")
+            logger.debug(
+                f"phd2 disconnect/connect took {
+                    humanfriendly.format_timespan(datetime.datetime.now(tz=datetime.UTC) - start)
+                }"
+            )
 
         logger.debug(f"telling guider to start '{sequence.tell_guider_to_start}'")
         if sequence.tell_guider_to_start == "loop":
